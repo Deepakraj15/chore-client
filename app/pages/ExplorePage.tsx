c4c01b8a51bf5e6616df7ae6cb84817419bc976c
Deepakraj15 <deepakraj15112002@gmail.com>
2025-04-03 12:42:24 +0530

    added new pages

---
+++ app/pages/ExplorePage.tsx
@@ -0,0 +1,10 @@
+import { Text,View } from "react-native";
+
+const ExplorePage = () =>{
+     return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>You are in Explore Page</Text>
+    </View>
+  );
+}
+export default ExplorePage;
---
+++ app/pages/SettingsPage.tsx
@@ -0,0 +1,11 @@
+import { Text,View } from "react-native";
+
+const SettingsPage = () =>{
+ return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>You are in settings page</Text>
+    </View>
+  );
+}
+
+export default SettingsPage;

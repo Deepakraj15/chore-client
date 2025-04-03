b4eb1437c416f9edf1b8a0bea7c90fb7b1957055
Deepakraj15 <deepakraj15112002@gmail.com>
2025-04-03 14:33:40 +0530

    added ProfilePage

---
+++ app/pages/ProfilePage.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+const ProfilePage = () =>{
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>You are in Profile Page</Text>
+    </View>
+  );
+}
+
+export default ProfilePage;
+

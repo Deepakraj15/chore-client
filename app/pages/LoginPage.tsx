1991de21a81f73724156c616f79ed389b0bdc57a
Deepakraj15 <deepakraj15112002@gmail.com>
2025-04-03 12:43:38 +0530

    removed pages

--- app/pages/LoginPage.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function LoginPage() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Welcome to Login Page</Text>
-    </View>
-  );
-}

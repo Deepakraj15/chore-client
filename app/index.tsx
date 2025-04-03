87902c851e964ef12399a076f16f69fb6526ad4a
Deepakraj15 <deepakraj15112002@gmail.com>
2025-04-03 12:43:28 +0530

    made changes to redirect app to home page upon opening

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,5 @@
-import { View, TouchableOpacity, Text } from "react-native";
-import { Link } from "expo-router";
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return (
-    <View style={{ width: 100, height: 40, backgroundColor: "blue", justifyContent: "center", alignItems: "center" }}>
-      <TouchableOpacity>
-        <Link href="./pages/home">
-          <Text style={{ color: "white" }}>Go to Home</Text>
-        </Link>
-      </TouchableOpacity>
-    </View>
-  );
+   return <Redirect href="/pages/HomePage" />;
 }

18e260a51d0d47cc000037467c003fbd642d1630
Deepakraj15 <deepakraj15112002@gmail.com>
2025-04-28 12:32:23 +0530

    made some changes to search bar

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,54 +1,26 @@
 import React, { useState } from "react";
-import { Icon, SearchIcon } from "@/components/ui/icon";
-import { Input, InputField, InputSlot, InputIcon } from "@/components/ui/input";
-import { Box } from "@/components/ui/box";
-import { StyleSheet } from "react-native";
+import { Box } from "@/components/ui/box"; 
+import { Input, InputIcon, InputField, InputSlot } from "@/components/ui/input";
+import { SearchIcon } from "@/components/ui/icon"; 
 
 const Search = () => {
   const [inputValue, setInputValue] = useState("");
 
   return (
-    <Box style={styles.container}>
-      <Input style={styles.inputWrapper}>
-        <InputSlot>
-          <InputIcon>
-            <Icon as={SearchIcon} size="md" color="#666" />
-          </InputIcon>
+    <Box className="w-full h-12 flex justify-center items-center">
+      <Input className="w-11/12 border-red-[100]" isFocused>
+        <InputSlot className="pl-2">
+          <InputIcon as={SearchIcon} />
         </InputSlot>
-        <InputField
+        <InputField onFocus={()=>{}}
+          type="text"
           placeholder="Search..."
-          placeholderTextColor="#999"
           value={inputValue}
           onChangeText={setInputValue}
-          style={styles.inputField}
         />
       </Input>
     </Box>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    height: 50,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10,
-  },
-  inputWrapper: {
-    flexDirection: "row",
-    width: "90%",
-    backgroundColor: "white",
-    borderRadius: 8,
-    paddingHorizontal: 10,
-  },
-  inputField: {
-    width: "100%",
-    fontSize: 16,
-    color: "#000",
-    borderColor: "black",
-    borderRadius: 5,
-    borderWidth: 1,
-  },
-});
 
 export default Search;

83bca6cfb71b5d0049b3f83c0476bfd236e7be2c
Deepakraj15 <deepakraj15112002@gmail.com>
2025-04-28 11:42:37 +0530

    renamed add post page to create Page

---
+++ app/pages/CreatePage.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+const CreatePage = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>You are in Explore Page</Text>
+    </View>
+  );
+}
+export default CreatePage;
